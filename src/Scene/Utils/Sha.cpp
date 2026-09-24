b498eec9d31349c03b385d700601c8e275c7c7c1
hypengw <hypengwip@gmail.com>
2026-09-24 11:21:03 +0000

    use licrypto sha1

--- src/Scene/Utils/Sha.cpp
+++ src/Scene/Utils/Sha.cpp
@@ -1,18 +1,9 @@
-module;
-#include <vog/sha1.hpp>
-
 module wescene.utils;
 import rstd;
-import rstd.cppstd;
+import licrypto;
 
 using namespace rstd::prelude;
 
 String utils::genSha1(slice<rstd::byte> input) {
-    SHA1 sha1;
-    if (! input.is_empty()) {
-        sha1.update(std::string(reinterpret_cast<const char*>(input.as_raw_ptr()),
-                                input.len().to_primitive()));
-    }
-    auto digest = sha1.final();
-    return rstd::into(rstd::cppstd::as_str(digest).unwrap());
+    return licrypto::sha1_hex(rstd::as_u8_slice(input));
 }
